872030c23ab844e2e730dc5a01c45c6df29826ee
Timmynat0r <dj_hollow@hotmail.com>
2025-03-13 01:41:33 +0100

    added pyinstaller dependency

--- better_build.bat
+++ better_build.bat
@@ -4,6 +4,9 @@ REM Batch file to build the Python script using PyInstaller
 REM Set the Python script name
 set SCRIPT_NAME=log_parser.py
 
+REM Install pyinstaller if needed
+pip install pyinstaller
+
 REM Run PyInstaller with the specified options
 pyinstaller --onefile --console %SCRIPT_NAME%
 
@@ -11,7 +14,7 @@ REM Check if the build was successful
 if %ERRORLEVEL% EQU 0 (
     echo Build successful! The executable is in the "dist" folder.
 ) else (
-    echo Build failed. Check the output for errors.
+    echo Build failed. Install Python 3.X and Check the output for errors.
 )
 
 REM Pause to keep the window open (optional)
